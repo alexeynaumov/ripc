e3e5af2fb9afcabcb4fbe961681b9aeea40c2841
Alexei Naumov <alexei.naumov@yahoo.com>
2016-09-04 14:20:07 +0300

    New threads. Initial commit.

--- fifo/fifo.py
+++ fifo/fifo.py
@@ -5,52 +5,62 @@ from io import BaseFIFOReader, BaseFIFOWriter
 class FIFOReader(QObject):
     def __init__(self, parent=None):
         QObject.__init__(self, parent)
-        self.__path = None
+        self.__baseFIFOReader = BaseFIFOReader()
 
-        class ReadingThread(QThread):
-            def __init__(self, parent):
-                QThread.__init__(self, parent)
+        class Worker(QObject):
+            def __init__(self, reader):
+                QObject.__init__(self)
+                self.__reader = reader
 
-            def run(self):
-                parent = self.parent()
-                reader = BaseFIFOReader()
-                reader.path = parent.path
-                reader.open()
-
-                QObject.connect(self, SIGNAL("read"), parent._FIFOReader__onReadyRead)
-
+            def read(self):
                 while True:
-                    data = reader.read()
+                    data = self.__reader.read()
                     self.emit(SIGNAL("read"), data)
 
-        self.__readingThread = ReadingThread(self)
         self.__on_read = None  # on-read callback
 
+        self.__thread = QThread(self)
+        self.__worker = Worker(self.__baseFIFOReader)
+
+        # READER AND WRITER LIVE IN THEIR OWN THREADS !!
+        self.__worker.moveToThread(self.__thread)
+
+        result = True
+        # TOUCH READER ONLY WITH SIGNALS !!
+        result &= QObject.connect(self.__worker, SIGNAL("read"), self.__onReadyRead)
+        # START THE READING THREAD WITH THE SIGNAL TOO !!
+        result &= QObject.connect(self.__thread, SIGNAL("started()"), self.__worker.read)
+
+        if not result:
+            raise RuntimeError("Error connecting signals to slots in FIFOReader.__init__")
+
     def __del__(self):
-        self.close()
+        self.__baseFIFOReader.close()
+        self.__thread.wait()
+        self.__thread.quit()
 
     def __onReadyRead(self, data):
         if self.__on_read:
             self.__on_read(data)
 
     def open(self):
-        self.__readingThread.start()
+        self.__baseFIFOReader.open()
+        self.__thread.start()
 
     def close(self):
-        self.__readingThread.wait()
-        self.__readingThread.quit()
+        self.__baseFIFOReader.close()
 
     @property
     def isOpen(self):
-        return self.__readingThread.isRunning()
+        return self.__baseFIFOReader.isOpen
 
     @property
     def path(self):
-        return self.__path
+        return self.__baseFIFOReader.path
 
     @path.setter
     def path(self, path):
-        self.__path = path
+        self.__baseFIFOReader.path = path
 
     @property
     def onRead(self):
@@ -64,17 +74,51 @@ class FIFOReader(QObject):
 class FIFOWriter(QObject):
     def __init__(self, parent=None):
         QObject.__init__(self, parent)
-
         self.__baseFIFOWriter = BaseFIFOWriter()
+
+        class Worker(QObject):
+            def __init__(self, writer):
+                QObject.__init__(self)
+                self.__writer = writer
+                self.__dataToWrite = ""
+
+            def write(self, data):
+                self.__dataToWrite += data
+
+                while self.__dataToWrite:
+                    char = self.__dataToWrite[0]
+
+                    self.__dataToWrite = self.__dataToWrite[1:]
+
+                    self.__writer.write(char)
+
+        self.__thread = QThread(self)
+        self.__worker = Worker(self.__baseFIFOWriter)
+
+        # READER AND WRITER LIVE IN THEIR OWN THREADS !!
+        self.__worker.moveToThread(self.__thread)
+
+        result = True
+        # TOUCH WRITER ONLY WITH SIGNALS !!
+        result &= QObject.connect(self, SIGNAL("write"), self.__worker.write)
+
+        if not result:
+            raise RuntimeError("Error connecting signals to slots in FIFOWriter.__init__")
+
+    def __del__(self):
+        self.__baseFIFOWriter.close()
+        self.__thread.wait()
+        self.__thread.quit()
 
     def open(self):
         self.__baseFIFOWriter.open()
+        self.__thread.start()
 
     def close(self):
         self.__baseFIFOWriter.close()
 
     def write(self, data):
-        self.__baseFIFOWriter.write(data)
+        self.emit(SIGNAL("write"), data)
 
     @property
     def isOpen(self):
